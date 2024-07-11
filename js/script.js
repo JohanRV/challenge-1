34c4431e5a6cd1d783182a7a23ddd9ffe95fabf0
JohanRv <172932835+JohanRV@users.noreply.github.com>
2024-07-11 08:08:04 -0400

    feat: update script.js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+//Terminar logica de encriptacion
+
+function encriptar() {
+    const texto = document.getElementById('input-text').value;
+    const resultado = document.getElementById('ouput-text');
+
+
+
+    //Pasar texto de entrada a la salida
+    resultado.value = texto;
+    
+}
