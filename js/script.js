4c4268aab6ee2381e92df4c0a0d83143107f6c88
JohanRv <172932835+JohanRV@users.noreply.github.com>
2024-07-11 12:20:50 -0400

    feat: comprobacion texto version 1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,50 @@
+//Valor del textArea de salida
+const resultado = document.getElementById('ouput-text');
+const array = [];
+//Funcion captura de texto del textArea de entrada
+function capturarTexto() {
+    return document.getElementById('input-text').value;
+}
+//Funcion de comprobacion del texto
+function verificarTexto() {
+    // Captura el texto una vez
+    const texto = capturarTexto();
+
+    // Expresiones regulares
+    const letrasMayusculas = /[A-Z]/;
+    const letrasAcentuadas = /[áéíóúÁÉÍÓÚüÜ]/;
+    const letrasCaracteresEspeciales = /[!\"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]/;
+
+    // Variables para verificar condiciones
+    let cumpleMayuscula = letrasMayusculas.test(texto);
+    let cumpleAcentuada = letrasAcentuadas.test(texto);
+    let cumpleEspecial = letrasCaracteresEspeciales.test(texto);
+
+    // Verifica si cumple con todas las condiciones simultáneamente
+    if (cumpleMayuscula && cumpleAcentuada && cumpleEspecial) {
+        console.log('El texto cumple con al menos una letra mayúscula, una letra acentuada y un caracter especial.');
+    } else {
+        console.log('El texto no cumple con todas las condiciones requeridas.');
+    }
+}
 
 
-//Terminar logica de encriptacion
+
+//Funcion de enviar el texto
+function enviarTexto(texto) {
+  return resultado.value = capturarTexto();
+}
 
 function encriptar() {
-    const texto = document.getElementById('input-text').value;
-    const resultado = document.getElementById('ouput-text');
-
-
-
-    //Pasar texto de entrada a la salida
-    resultado.value = texto;
+    verificarTexto();
+    enviarTexto();
     
 }
+
+/*
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+*/
