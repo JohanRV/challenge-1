1db5212f3536b1494d9ab9e0293e65c2935c01ce
JohanRv <172932835+JohanRV@users.noreply.github.com>
2024-07-12 07:52:52 -0400

    fix: function desencriptar corregida

--- js/script.js
+++ js/script.js
@@ -47,13 +47,13 @@ function encriptarTexto() {
 
 function desencriptarTexto() {
     let texto = capturarTexto();
-    
 
-    texto = texto.replaceAll('ai', 'a');
-    texto = texto.replaceAll('enter', 'e');
-    texto = texto.replaceAll('imes', 'i');
-    texto = texto.replaceAll('ober', 'o');
+
     texto = texto.replaceAll('ufat', 'u');
+    texto = texto.replaceAll('ober', 'o');
+    texto = texto.replaceAll('imes', 'i');
+    texto = texto.replaceAll('enter', 'e');
+    texto = texto.replaceAll('ai', 'a');
 
     return texto;
 }
